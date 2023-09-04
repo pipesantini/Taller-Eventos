e112d208ffaa6782d87a8bfad37972457e33d3f2
pipesantini <pipesantini@gmail.com>
2023-09-03 21:01:26 -0300

    first commit

---
+++ eventos.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const contenedor = document.getElementById("div");
+
+    contenedor.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+
+});
+
+
